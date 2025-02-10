7cc1dbe93c48fab097eb67275740f4abdaab4155
stecuproductions <stecu03@gmail.com>
2025-02-10 14:35:04 +0100

    poprawka gpages

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom'; // Importuj BrowserRouter
+import { HashRouter } from 'react-router-dom'; // Importuj BrowserRouter
 import App from './App';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter base="/https://github.com/stecuproductions/torebki"> {/* Otaczamy App komponentem BrowserRouter */}
+    <HashRouter base="/https://github.com/stecuproductions/torebki"> {/* Otaczamy App komponentem BrowserRouter */}
       <App />
-    </BrowserRouter>
-  </StrictMode>
+    </HashRouter>
 );
