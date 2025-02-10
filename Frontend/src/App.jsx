dff7a526d535a1ff89f0135bac270091033817e7
stecuproductions <stecu03@gmail.com>
2025-02-10 14:55:23 +0100

    Poprawka do vercela

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="https://stecuproductions.github.io/torebki/" element={<Home />} />
       </Routes>
     </>
   );
