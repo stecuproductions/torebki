09c8c28de3a13be943e422b87350c073efe1f756
stecuproductions <stecu03@gmail.com>
2025-02-10 15:03:16 +0100

    zmiana sciezek do zdjec

--- Frontend/src/views/Home.jsx
+++ Frontend/src/views/Home.jsx
@@ -4,22 +4,22 @@ import Product from "./Product.jsx";
 //            <img src="public/images/_MG_0295.JPG" alt="" />
 function Home(){
     const produkty=[
-        {id:0, nazwa: "Torebka Denim Czarno szara", cena: "1000,00", zdjecie: "public/images/_MG_0508.JPG"},
-        {id:0, nazwa: "Torebka Denim Szara", cena: "800,00", zdjecie: "public/images/_MG_0078.JPG"},
+        {id:0, nazwa: "Torebka Denim Czarno szara", cena: "1000,00", zdjecie: "/images/_MG_0508.JPG"},
+        {id:1, nazwa: "Torebka Denim Szara", cena: "800,00", zdjecie: "/images/_MG_0078.JPG"},
 
     ]
     return (
         <>
            <section>
                 <div className='s1'>
-                    <img src="public/images/_MG_0295.JPG" alt=""  className='s1-i1'/>
+                    <img src="/images/_MG_0295.JPG" alt=""  className='s1-i1'/>
                     <h1 className='s1-h1'>WITAJ!</h1>
                     <div className='s1-text'>
                         <p>Witaj na naszej stronie</p>
                         <p className='s1-text-p2'>Odkryj unikalne torebki denimowe tworzone z pasją przez naszych projektantów</p>
                     </div>
-                    <img className='s1-i2' src="public/images/_MG_0327.JPG" alt="" />
-                    <img src="public/images/_MG_0144.JPG" className='s1-i3' alt="" />
+                    <img className='s1-i2' src="/images/_MG_0327.JPG" alt="" />
+                    <img src="/images/_MG_0144.JPG" className='s1-i3' alt="" />
                 </div>
            </section>
            <section>
